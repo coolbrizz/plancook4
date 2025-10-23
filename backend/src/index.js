1bc5974e3e4e6301acbc2a2d61c3ce20c7d81f82
antony Auvray <antony.auvray@hotmail.com>
2025-10-23 14:49:44 +0200

    modif packege

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-dotenv.config();
 const app = express();
 
 // Middleware
